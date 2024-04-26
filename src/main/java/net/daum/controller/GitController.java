e9a7f52e8dfd79839b162aa8f2c787c3d4d2f764
yoo-cheol <tjddbcjf26@gmail.com>
2024-04-26 15:55:59 +0900

    세번째

--- src/main/java/net/daum/controller/GitController.java
+++ src/main/java/net/daum/controller/GitController.java
@@ -1,5 +1,19 @@
 package net.daum.controller;
 
-public class GitController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class GitController {
+	
+	@GetMapping("/git_test")
+	public ModelAndView git_test() {
+		
+		int[] javaArr= {100,100,100};
+		
+		ModelAndView gm = new ModelAndView("git_test");
+		gm.addObject("javaArr", javaArr);
+		return gm;
+	}// git_test()
 }
